50a393b4441497444b26423be15bb7c0d907c715
Bartosz <radu480@o2.pl>
2021-08-18 12:07:49 +0200

    Prepared layout for sign in page.

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,7 +1,28 @@
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  root: {
+    height: "100%",
+  },
+});
+
 const SignInPage = () => {
-  return <span>Sign In Page</span>;
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={classes.root}
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <span>Sign In Page</span>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default SignInPage;
